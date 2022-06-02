26f79eca1ff43c634e212cb6d0c5957cd48af7f3
Janus Hasseriis <janus@hasseriis.net>
2022-06-02 23:14:59 +0200

    New base for split boxes

--- split-boxes/sketch.js
+++ split-boxes/sketch.js
@@ -1,54 +1,112 @@
+// https://gorillasun.de/blog/an-algorithm-for-irregular-grids
+
+let rectInfo = [];
+
 function setup() {
-  // var c = document.getElementById("doodle");
-  c = createCanvas(500, 500);
-  //var ctx = c.getContext("2d");
+  randomSeed(0);
 
-  //ctx.beginPath();
-  //ctx.lineWidth = 4;
+  w = min(windowWidth, windowHeight);
+  createCanvas(w, w);
 
-  var xPad = Math.floor(c.width * 0.1);
-  var yPad = Math.floor(c.height * 0.1);
+  // size of the padding between grid and sketch borders
+  padding = w / 12;
 
-  var initialRect = new Rectangle(
-    new Point(0, 0),
-    new Point(c.width, c.height)
-  );
+  // number of rows and columns of the grid
+  gridDivsX = 15;
+  gridDivsY = 15;
 
-  initialRect.split(xPad, yPad, 0, 5, ctx);
+  // actual spacing between grid points
+  gridSpacingX = (w - padding * 2) / gridDivsX;
+  gridSpacingY = (w - padding * 2) / gridDivsY;
+
+  // here we populate the 2d boolean array
+  bools = [];
+
+  for (let x = 0; x < gridDivsX; x++) {
+    var column = [];
+    for (let y = 0; y < gridDivsY; y++) {
+      column.push(1);
+    }
+    bools.push(column);
+  }
+
+  constructIrregularGrid([2, 3]);
+  constructIrregularGrid([1]);
+
+  background(0);
+  stroke(255);
+  strokeWeight(4);
+  noFill();
+  drawGrid();
+  markEmptySpots();
 }
 
-function draw() {}
+function makeRect(posX, posY, dimX, dimY) {
+  this.posX = posX;
+  this.posY = posY;
+  this.dimX = dimX;
+  this.dimY = dimY;
+}
 
-class Rectangle {
-  split(xPad, yPad, depth, limit, ctx) {
-    ctx.fillStyle = colors[randInt(0, colors.length)];
-    ctx.fillRect(this.min.x, this.min.y, this.max.x, this.max.y);
-    this.draw(ctx);
+function constructIrregularGrid(sizesArr) {
+  for (let x = 0; x < gridDivsX - max(sizesArr) + 1; x++) {
+    for (let y = 0; y < gridDivsY - max(sizesArr) + 1; y++) {
+      xdim = random(sizesArr);
+      ydim = random(sizesArr);
 
-    // Check the level of recursion
-    if (depth == limit) {
-      return;
+      fits = true;
+
+      // check if within bounds
+      if (x + xdim > gridDivsX || y + ydim > gridDivsY) {
+        fits = false;
+      }
+
+      // check if rectangle overlaps with any other rectangle
+      if (fits) {
+        for (let xc = x; xc < x + xdim; xc++) {
+          for (let yc = y; yc < y + ydim; yc++) {
+            if (bools[xc][yc] == 0) {
+              fits = false;
+            }
+          }
+        }
+      }
+
+      if (fits) {
+        // mark area as occupied
+        for (let xc = x; xc < x + xdim; xc++) {
+          for (let yc = y; yc < y + ydim; yc++) {
+            bools[xc][yc] = false;
+          }
+        }
+
+        rectInfo.push(new makeRect(x, y, xdim, ydim));
+      }
     }
+  }
+}
 
-    // Check the rectangle is enough large and tall
-    if (this.width < 2 * xPad || this.height < 2 * yPad) {
-      return;
+function drawGrid() {
+  for (let n = 0; n < rectInfo.length; n++) {
+    r = rectInfo[n];
+    rect(
+      r.posX * gridSpacingX + padding,
+      r.posY * gridSpacingY + padding,
+      r.dimX * gridSpacingX,
+      r.dimY * gridSpacingY
+    );
+  }
+}
+
+function markEmptySpots() {
+  for (let x = 0; x < gridDivsX; x++) {
+    for (let y = 0; y < gridDivsY; y++) {
+      if (bools[x][y]) {
+        point(
+          x * gridSpacingX + gridSpacingX / 2 + padding,
+          y * gridSpacingY + gridSpacingY / 2 + padding
+        );
+      }
     }
-
-    // If the rectangle is wider than it's height do a left/right split
-    if (this.width > this.height) {
-      var x = randInt(this.min.x + xPad, this.max.x - xPad);
-      var r1 = new Rectangle(this.min, new Point(x, this.max.y));
-      var r2 = new Rectangle(new Point(x, this.min.y), this.max);
-      // Else do a top/bottom split
-    } else {
-      var y = randInt(this.min.y + yPad, this.max.y - yPad);
-      var r1 = new Rectangle(this.min, new Point(this.max.x, y));
-      var r2 = new Rectangle(new Point(this.min.x, y), this.max);
-    }
-
-    // Split the sub-rectangles
-    r1.split(xPad, yPad, depth + 1, limit, ctx);
-    r2.split(xPad, yPad, depth + 1, limit, ctx);
   }
 }
