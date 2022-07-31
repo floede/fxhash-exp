7fe2b45c57ababefb951894566ae0a435699282b
Janus Hasseriis <janus.hasseriis@tryg.dk>
2022-07-31 22:23:20 +0200

    Adding several shapes

--- simplelines/sketch.js
+++ simplelines/sketch.js
@@ -7,9 +7,11 @@ let canvasWidth;
 const format = "wide"; //Math.random() > 0.5 ? "wide" : "high";
 const referenceSize = format === "wide" ? 1000 : 500;
 const aspect = format === "wide" ? 2 / 1 : 1 / 2;
+const golden = (1 + Math.sqrt(5)) / 2;
 const noOfLines = format === "wide" ? 40 : 20;
 let lineWidth;
 let lineFills = [];
+let shapes;
 
 const palette = colors[3];
 const mappedCol = false;
@@ -21,12 +23,14 @@ function setup() {
 
   c = createCanvas(w, h);
   //layerSquares = createGraphics(w, h);
+  angleMode(DEGREES);
   colorMode(HSB);
   //layerSquares.colorMode(HSB);
   background(100, 0, 100, 0);
   filter(BLUR, 5);
-  noFill();
-  noStroke();
+  //noFill();
+  //noStroke();
+  stroke(100);
   if (format === "wide") {
     lineFills.push(new LineFill(0, height / 2, palette));
     lineFills.push(new LineFill(height / 2, height, palette));
@@ -45,6 +49,7 @@ function setup() {
   lineFills.forEach((fill) => {
     fill.setColors();
   });
+  shapes = "diamond"; //random(["diamond", "round", "ellipse", "square"]);
 }
 
 function draw(params) {
@@ -76,24 +81,26 @@ function draw(params) {
 
   if (format === "wide") {
     push();
-    let firstDiamond = new Diamond(0, 0, width / 2, height);
-    firstDiamond.show();
+    fill(100);
+    let firstShape = getShape(shapes, 0, 0, width / 2, height);
+    firstShape.show();
 
     drawingContext.clip();
     lineFills[2].show();
     pop();
 
     push();
-    let secondDiamond = new Diamond(width / 2, 0, width / 2, height);
-    secondDiamond.show();
+    fill(100);
+    let secondShape = getShape(shapes, width / 2, 0, width / 2, height);
+    secondShape.show();
 
     drawingContext.clip();
     lineFills[3].show();
     pop();
   } else {
     push();
-    let firstDiamond = new Diamond(0, 0, width, height / 2);
-    firstDiamond.show();
+    let firstShape = getShape(shapes, 0, 0, width, height / 2);
+    firstShape.show();
 
     drawingContext.clip();
     lineFills[3].show();
@@ -101,8 +108,8 @@ function draw(params) {
 
     push();
     fill(100);
-    let secondDiamond = new Diamond(0, height / 2, width, height / 2);
-    secondDiamond.show();
+    let secondShape = getShape(shapes, 0, height / 2, width, height / 2);
+    secondShape.show();
 
     drawingContext.clip();
     lineFills[4].show();
@@ -137,6 +144,10 @@ class LineFill {
       strokeWeight(lineWidth);
       stroke(this.lineCols[index]);
       line(x, this.y, x, this.h);
+      //stroke(100);
+      //strokeWeight(5 * windowScale);
+      //fill(this.lineCols[index]);
+      //rect(x, this.y, lineWidth, this.h);
     }
   }
 }
@@ -150,18 +161,94 @@ class Diamond {
   }
 
   show() {
-    //stroke(100);
-    //point(this.x, this.h / 2);
-    //point(this.x + this.w / 2, this.y);
-    //point(this.x + this.w, this.h / 2);
-    //point(this.x + this.w / 2, this.h);
-
     beginShape();
     vertex(this.x, this.h / 2 + this.y);
     vertex(this.x + this.w / 2, this.y);
     vertex(this.x + this.w, this.h / 2 + this.y);
     vertex(this.x + this.w / 2, this.h + this.y);
     endShape(CLOSE);
+  }
+}
+
+class Round {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+
+  show() {
+    ellipseMode(CORNER);
+    ellipse(this.x, this.y, this.w, this.h);
+  }
+}
+
+class SuperEllipse {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.n = 2.5;
+    this.a = this.w / 2;
+    this.b = this.h / 2;
+  }
+  show() {
+    push();
+    translate(this.x + this.w / 2, this.y + this.h / 2);
+    beginShape();
+    for (let t = 0; t <= 360; t += 5) {
+      let x = pow(abs(cos(t)), 2 / this.n) * this.a * sgn(cos(t));
+      let y = pow(abs(sin(t)), 2 / this.n) * this.b * sgn(sin(t));
+      vertex(x, y);
+    }
+    endShape(CLOSE);
+    pop();
+  }
+}
+
+class Square {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+
+  show() {
+    rectMode(CENTER);
+    rect(
+      this.x + this.w / 2,
+      this.y + this.h / 2,
+      this.w - 8 * lineWidth,
+      this.h - 8 * lineWidth
+    );
+  }
+}
+
+function sgn(w) {
+  if (w < 0) {
+    return -1;
+  } else if (w === 0) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+function getShape(shape, x, y, w, h) {
+  switch (shape) {
+    case "diamond":
+      return new Diamond(x, y, w, h);
+    case "round":
+      return new Round(x, y, w, h);
+    case "ellipse":
+      return new SuperEllipse(x, y, w, h);
+    case "square":
+      return new Square(x, y, w, h);
+    default:
+      break;
   }
 }
 
@@ -175,19 +262,17 @@ function windowResized() {
 }
 
 function setDimensions() {
-  console.log("TEST: ", aspect, w, h);
-  // This is how we constrain the canvas to the smallest dimension of the window
   if (aspect === 1) {
-    w = h = min(windowWidth, windowHeight);
+    w = h = floor(min(windowWidth, windowHeight) / noOfLines) * noOfLines;
   } else if (aspect > 1) {
-    w = min(windowWidth, windowHeight * aspect);
+    w = floor(min(windowWidth, windowHeight * aspect) / noOfLines) * noOfLines;
     h = w / aspect;
-    console.log("TEST: ", w, h);
   } else if (aspect < 1) {
-    h = min(windowWidth / aspect, windowHeight);
+    h =
+      floor(min(windowWidth / aspect, windowHeight) / (2 * noOfLines)) *
+      2 *
+      noOfLines;
     w = h * aspect;
-    console.log("TEST: ", w, h);
   }
   windowScale = map(w, 0, referenceSize, 0, 1);
-  console.log("SCALE: ", windowScale);
 }
