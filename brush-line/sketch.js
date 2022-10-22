98387441bbf394d1a2c20123b775ae9a968d7a15
Janus Hasseriis <janus.hasseriis@tryg.dk>
2022-10-22 11:16:29 +0200

    Randomize pos

--- brush-line/sketch.js
+++ brush-line/sketch.js
@@ -6,29 +6,41 @@ function setup() {
   w = h / aspect;
 
   c = createCanvas(w, h);
+  colorMode(HSB);
 }
 
 function draw() {
   background(100);
-  let line = new WideLine(100, 200, 500, 1);
+  let line = new WideLine([0, 100, 100], 20, 400, 1, 25);
   line.drawLines();
   noLoop();
 }
 
 class WideLine {
-  constructor(color, width, height, strokeWidth) {
+  constructor(color, width, height, strokeWidth, noOfStrokes) {
     this.color = color;
     this.width = width;
     this.height = height;
     this.strokeWidth = strokeWidth;
-    this.noOfStrokes = Math.ceil(width / strokeWidth);
+    this.noOfStrokes = noOfStrokes;
   }
   drawLines() {
+    const colOffset = 6;
+    const posOffset = 3;
     strokeCap(SQUARE);
     strokeWeight(this.strokeWidth);
     for (let index = 0; index < this.noOfStrokes; index++) {
-      stroke(random(100));
-      line(index * this.strokeWidth, 0, index * this.strokeWidth, this.height);
+      stroke(
+        random(-colOffset, colOffset) + this.color[0],
+        random(-colOffset, colOffset) + this.color[1],
+        random(-colOffset, colOffset) + this.color[2]
+      );
+      line(
+        index * this.strokeWidth,
+        0 + random(posOffset),
+        index * this.strokeWidth,
+        this.height - random(posOffset)
+      );
     }
   }
 }
